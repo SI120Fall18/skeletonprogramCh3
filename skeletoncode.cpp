18dbd8fb732ecdf5684dccc5f96ad4ecbd87ae1a
Allen Sarmiento <42989060+Allen-Sarmiento@users.noreply.github.com>
2018-09-24 15:49:24 -0700

    Update skeletoncode.cpp

--- skeletoncode.cpp
+++ skeletoncode.cpp
@@ -10,13 +10,15 @@ int main()
 	// Write code here that prompts the user
 	// to enter how many hours they studied over the weekend
 	// and reads the input into the hours variable.
+	std::cout << "How many hours did you study over the weekend?\n";
+	std::cin >> hours;
 
 	// The following line does the conversion.
 	minutes = hours * 60;
 
 	// Write code here that displays the amount of
 	// minutes spent studying.
-
+	std::cout << "Minutes studied: " << minutes << std::endl;
 
 	return 0;
 }
