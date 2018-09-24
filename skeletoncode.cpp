c519792f6a83d3f271dd621478ec6bcda178742e
Allen Sarmiento <42989060+Allen-Sarmiento@users.noreply.github.com>
2018-09-23 23:23:37 -0700

    Add files via upload

---
+++ skeletoncode.cpp
@@ -0,0 +1,22 @@
+// Complete the following program skeleton.
+// This is a modified program from the textbook.
+
+#include <iostream>
+
+int main() 
+{
+	double hours, minutes;
+
+	// Write code here that prompts the user
+	// to enter how many hours they studied over the weekend
+	// and reads the input into the hours variable.
+
+	// The following line does the conversion.
+	minutes = hours * 60;
+
+	// Write code here that displays the amount of
+	// minutes spent studying.
+
+
+	return 0;
+}
